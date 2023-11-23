984d647a88cdf7dafc12795b01aa1fe3f73183d4
tung <tung.nguyen@rits.center>
2023-11-23 16:15:27 +0700

    submit

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # ImportGenius assignment
 
-Essay: `essay.txt`
+**Essay**: `essay.txt`
 
-Answer Notebook: ``notebooks/Skills Test Data Scientist.ipynb`
+**Answer notebook**: ``notebooks/Skills Test Data Scientist.ipynb`
+
+Execution steps:
+
+    1. create and activate a virtual environment with python 3.9. Give it a nice name e.g. `genius`
+
+    2. Install the environment into jupyter kernel
+
+    ```bash
+    python -m ipykernel install --user --name genius # replace with your actual env name
+    ```
+
+    3. Install dependencies into your environemnt
+    ```bash
+    pip install -r requirements.txt
+    ```
+    4. Copy `main.csv`, `ports.csv`, `importer_normalized_data.csv` to `datasets` folder
+
+    5. Execute the **answer notebook**
+
+
--- utils/pretty.py
+++ utils/pretty.py
@@ -1,7 +1,8 @@
-from IPython.display import display, Markdown
-import pandas as pd
-import matplotlib.pyplot as plt
 from typing import Optional
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from IPython.display import Markdown, display
 
 
 def pprint(content):
@@ -11,12 +12,12 @@ def pprint(content):
     Parameters:
         content (str): The Markdown content to be displayed.
     """
-    display(Markdown(content)) 
+    display(Markdown(content))
 
 
-def plot_missing_values(df: pd.DataFrame,
-                        as_percentage: Optional[bool] = False,
-                        figsize=(10,6)) -> None:
+def plot_missing_values(
+    df: pd.DataFrame, as_percentage: Optional[bool] = False, figsize=(10, 6)
+) -> None:
     """
     Plot a horizontal stacked bar chart showing the number of missing values per column.
 
@@ -43,68 +44,114 @@ def plot_missing_values(df: pd.DataFrame,
         non_missing_values_percentage = non_missing_values
 
     fig, ax = plt.subplots(figsize=figsize)
-    
+
     # Plot non-missing values in black on the left
-    ax.barh(missing_values.index, non_missing_values_percentage, color='black', label='Non-Missing')
+    ax.barh(
+        missing_values.index,
+        non_missing_values_percentage,
+        color="black",
+        label="Non-Missing",
+    )
 
     # Plot missing values in white on the right
-    ax.barh(missing_values.index, missing_values_percentage, left=non_missing_values_percentage, color='white', edgecolor='black', label='Missing')
+    ax.barh(
+        missing_values.index,
+        missing_values_percentage,
+        left=non_missing_values_percentage,
+        color="white",
+        edgecolor="black",
+        label="Missing",
+    )
 
-    ax.set_xlabel('Number of Values' if not as_percentage else 'Percentage of Values')
-    ax.set_ylabel('Columns')
-    ax.set_title('Missing Values per Column')
+    ax.set_xlabel("Number of Values" if not as_percentage else "Percentage of Values")
+    ax.set_ylabel("Columns")
+    ax.set_title("Missing Values per Column")
     ax.legend()
 
     # Show the plot
     plt.tight_layout()
     plt.show()
-    
-    
-    
+
+
 def plot_value_counts(value_counts, k=20, plot_title=""):
     fig, ax = plt.subplots()
     ax.set_title(plot_title)
 
     # Take the top k value counts
     total = value_counts.sum()
-    
+
     value_counts = value_counts.head(k)
 
     # Calculate percentages
     percentages = (value_counts / total) * 100
-    
+
     # Plot horizontal bar chart without bounding box
     bars = value_counts[::-1].plot.barh()
 
     # Annotate each bar with the percentage
     for bar, percentage in zip(bars.patches, percentages[::-1]):
-        ax.text(bar.get_width(), bar.get_y() + bar.get_height() / 2, f'{percentage:.2f}%', 
-                va='center', ha='left', color='black')
+        ax.text(
+            bar.get_width(),
+            bar.get_y() + bar.get_height() / 2,
+            f"{percentage:.2f}%",
+            va="center",
+            ha="left",
+            color="black",
+        )
 
     # Remove the bounding box
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_visible(False)
-    ax.spines['left'].set_visible(False)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(False)
+    ax.spines["left"].set_visible(False)
 
     plt.show()
 
-    
+
 def generate_sample_history(main, query=None):
     """
     Generate a sample transactional history of ONE random importer
     Optional: provide a query to narrow down sampling pool
     """
     if query is None:
-        sample_importer = list(main['importer_deduped_name'].drop_duplicates().sample(1))[0]
+        sample_importer = list(
+            main["importer_deduped_name"].drop_duplicates().sample(1)
+        )[0]
     else:
-        sample_importer = list(main.query(query)['importer_deduped_name'].drop_duplicates().sample(1))[0]
+        sample_importer = list(
+            main.query(query)["importer_deduped_name"].drop_duplicates().sample(1)
+        )[0]
     print(sample_importer)
-    sample_history = main[main["id"]==sample_importer]
-    return (
-        sample_history[['file_month', 'hs_codes', 'calculated_teu',
-                        'country_of_origin', 'port_of_unlading', 
-                        'importer_deduped_name', 'importer_rank', 'id']].sort_values(
-        ['file_month', 'hs_codes']
-        )
-        )
+    sample_history = main[main["id"] == sample_importer]
+    return sample_history[
+        [
+            "file_month",
+            "hs_codes",
+            "calculated_teu",
+            "country_of_origin",
+            "port_of_unlading",
+            "importer_deduped_name",
+            "importer_rank",
+            "id",
+        ]
+    ].sort_values(["file_month", "hs_codes"])
+
+
+def classification_report_df(
+    report: dict, model_name: str, n_classes=4
+) -> pd.DataFrame:
+    """
+    From the sklearn classification report, convert a long format table
+    """
+
+    classes = [str(i) for i in range(n_classes)]
+    metrics = ["precision", "recall", "f1-score"]
+    report_df = (
+        pd.DataFrame(report)[classes]
+        .loc[metrics]
+        .reset_index()
+        .melt(id_vars="index", value_name=model_name)
+    )
+    report_df["metric"] = report_df["index"] + "_" + report_df["variable"]
+
+    return report_df[["metric", model_name]].sort_values("metric")
